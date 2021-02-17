30086b436e87a879407eb5bea84e104e77f64de8
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-17 20:04:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +13,11 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), new BigDecimal(3000), obj);
+		
+		System.out.println(seller);
+		
 		
 	}
 }
