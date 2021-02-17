39c61d0614e4c7a1fe208e604d9d57c5ec695b6d
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-17 20:16:18 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DAOFactory.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DAOFactory {
+
+	public static SellerDAO createSellerDAO() {
+		return new SellerDaoJDBC();
+	}
+}
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDAO{
+
+	@Override
+	public void insert(Seller dep) {
+		
+	}
+
+	@Override
+	public void update(Seller dep) {
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+}
